13f9e6a715498684505ae51a0559855e097d5475
Junio <juniox1997@gmail.com>
2026-02-22 20:29:00 -0300

    Delete convertFromChdToCue.sh

--- convertFromChdToCue.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Grab .chd file
-for f in PS1/*/*.chd
-do
-	name=${f%.chd} # Remove '.chd' from file name
-	chdman extractcd -i "$name.chd" -o "$name.cue" -ob "$name.iso" --force
-done
